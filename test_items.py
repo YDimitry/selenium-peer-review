048c9787e2c05d88d48be5771371a661cbc0bc98
Dm <YDimitry@mail.ru>
2020-01-31 00:12:00 +0300

    закомм.time.sleep

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_button_add_to_basket(browser):
     browser.get(link)
-    time.sleep(10)
+    # time.sleep(10)
     btn = browser.find_element_by_css_selector('button.btn-add-to-basket')
     assert btn
 
